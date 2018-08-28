3e32edebfb6db80b12b3bf8500ed8ddc48912537
Philip Nowak <phillber9@gmail.com>
2018-08-28 16:34:18 -0500

    Add project files.

---
+++ MLM.Audition/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MLM.Audition.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
